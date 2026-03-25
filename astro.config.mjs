072a564f94c58a18c2ca15b120e7d1008c5bf565
LaileF <lailefletcher@gmail.com>
2026-03-25 16:48:47 +0000

    fix build - revert to static

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,8 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import node from "@astrojs/node";
 
 export default defineConfig({
   site: "https://signalroutes.work",
-  output: "server",
-  adapter: node({
-    mode: "standalone",
-  }),
   integrations: [mdx(), sitemap()],
 });
